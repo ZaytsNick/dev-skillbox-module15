c327dddd718ee68e1071f2e5f79f158285e35625
Zayts Work <zaytsev@un74.ru>
2023-10-26 11:33:33 +0500

    task#1 start

---
+++ task#1/main1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> a={-2,1,-3,4,-1,2,1,-5,4};
+    for(int i=0;i<a.size();++i)
+    {
+        
+    }
+}
