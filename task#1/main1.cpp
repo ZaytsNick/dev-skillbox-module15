90fa33b99e46748dd829d856a4aa9528dae27b29
Zayts Work <zaytsev@un74.ru>
2023-10-26 16:04:28 +0500

    task#1 cycle

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
-    vector<int> a={-2,1,-3,4,-1,2,1,-5,4};
-    for(int i=0;i<a.size();++i)
+    vector<int> a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int index_start, index_end;
+    for (int i = 0; i < a.size(); ++i)
     {
-        
+        if (a[i] >a[i+1])
+        {
+            for (int j = i; j < a.size(); ++j)
+            {
+                
+            }
+        }
     }
 }
