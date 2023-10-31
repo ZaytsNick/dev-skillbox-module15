6bd87fa2678cd877a1d9de862e43734f4974cbf9
Zayts Work <zaytsev@un74.ru>
2023-10-31 11:26:05 +0500

    task#1 complite

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -5,15 +5,31 @@ using namespace std;
 int main()
 {
     vector<int> a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int index_start, index_end;
+    for(int i = 0; i < a.size(); ++i)
+    {
+        cout<<a[i]<<" ";
+    }
+    int index_start, index_end, sum;
     for (int i = 0; i < a.size(); ++i)
     {
-        if (a[i] >a[i+1])
+        int temp_sum(0);
+        if (a[i] > a[i + 1])
         {
             for (int j = i; j < a.size(); ++j)
             {
-                
+                temp_sum+=a[j];
+               // sum?0:sum=temp_sum-1;
+                if(temp_sum>sum)
+                {
+                    sum=temp_sum;
+                    index_start=i;
+                    index_end=j;
+                }
             }
+
         }
+
+        // if(temp_sum>sum)
     }
+    cout<< "\nпервое число: "<<index_start<<"\nпоследнее число: "<<index_end<<endl;
 }
