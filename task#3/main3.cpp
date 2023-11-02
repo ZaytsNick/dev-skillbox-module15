30b624672a24c41c27f4a78f9ea02157ad90ad27
ZaytsNick <10kcin01@gmail.com>
2023-11-02 22:18:08 +0500

    task#4 ?

--- task#3/main3.cpp
+++ task#3/main3.cpp
@@ -2,34 +2,49 @@
 #include <vector>
 using namespace std;
 
-
 int main()
 {
-    vector<int> vect;
+    vector<int> vect[5];
     int number;
+
     while (number != -2)
     {
         cout << "input: ";
         cin >> number;
-        if (number == -1)
+        if (vect[4] > number)
         {
-            for (int j = 0; j < vect.size(); j++)
+            vect[4] = number;
+            for (int i(4); i > 0; --i)
             {
-                for (int i = 0; i < vect.size() - 1; i++)
+                if (vect[i] < vect[i - 1])
                 {
-                    if (vect[i] > vect[i + 1])
-                        swap(vect[i], vect[i + 1]);
-                    
+                    swap(vect[i], vect[i - 1]);
+                }
+                else
+                {
+                    break;
                 }
             }
-            cout << "conclusion: " << vect[4] << "(in sorted form the array looks like this: {";
-            for (int i = 0; i < vect.size(); ++i)
-            {
-                cout << vect[i] << " ";
-            }
-            cout << "})";
         }
-        else
-            vect.push_back(number);
+        if (number == -1)
+        {
+            cout << "conclusion: " << vect[4]<<endl;
+
+            // for (int j = 0; j < vect.size(); j++)
+            // {
+            //     for (int i = 0; i < vect.size() - 1; i++)
+            //     {
+            //         if (vect[i] > vect[i + 1])
+            //             swap(vect[i], vect[i + 1]);
+
+            //     }
+            // }
+            // cout << "conclusion: " << vect[4] << "(in sorted form the array looks like this: {";
+            // for (int i = 0; i < vect.size(); ++i)
+            // {
+            //     cout << vect[i] << " ";
+            // }
+            // cout << "})";
+        }
     }
 }
