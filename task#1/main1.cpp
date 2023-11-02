f35c9d5dfc16f0dc6945821bddf6147824f0d17e
ZaytsNick <10kcin01@gmail.com>
2023-11-02 21:21:29 +0500

    task#1 revision

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    vector<int> a = {-2, -1, -3, -4};
     for (int i = 0; i < a.size(); ++i)
     {
         cout << a[i] << " ";
@@ -13,17 +13,15 @@ int main()
     for (int i = 0; i < a.size(); ++i)
     {
         int temp_sum(0);
-        if (a[i] > a[i + 1])
+
+        for (int j = i; j < a.size(); ++j)
         {
-            for (int j = i; j < a.size(); ++j)
+            temp_sum += a[j];
+            if (temp_sum > (sum==0?sum=temp_sum:sum))
             {
-                temp_sum += a[j];
-                if (temp_sum > sum)
-                {
-                    sum = temp_sum;
-                    index_start = i;
-                    index_end = j;
-                }
+                sum = temp_sum;
+                index_start = i;
+                index_end = j;
             }
         }
     }
